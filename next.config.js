3a7f0724234a5934887c431f0e5d93610e6b89e8
K. Singh <16361374+yourskiss@users.noreply.github.com>
2024-09-12 15:56:41 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 const withPWA = require('next-pwa')({
-
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-    rewritePaths: [
-      {
-        from: /^\/$/,
-        to: '/assets/offline.html',
-      },
-    ],
-
-
+    disable:  process.env.NODE_ENV === 'development',
   });
   
  const createNextIntlPlugin = require('next-intl/plugin');
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,3 @@
- 
-
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 export default createMiddleware(routing);
