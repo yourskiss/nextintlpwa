93bb5b386b35d0cca8b10dc54eb874a6f9b7d14d
K. Singh <16361374+yourskiss@users.noreply.github.com>
2024-09-12 11:39:07 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
-    disable: false, // process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'development',
     register: true,
     scope: '/app',
-    sw: 'sw.js',
+   // sw: 'sw.js',
     /* runtimeCaching: [
         {
           urlPattern: /^https:\/\/example\.com\/.*\.(png|jpg|jpeg|svg|gif)$/,
