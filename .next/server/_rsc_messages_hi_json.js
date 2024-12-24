895cf9cd9a085c128e62e3c8f4c546e7ef3527bb
yourskiss <ksingh1817@gmail.com>
2024-12-25 01:18:24 +0530

    first commit

---
+++ .next/server/_rsc_messages_hi_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_hi_json";
+exports.ids = ["_rsc_messages_hi_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/hi.json":
+/*!**************************!*\
+  !*** ./messages/hi.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"Navigation":{"english":"अंग्रेज़ी","hindi":"हिन्दी"},"HomePage":{"title":"होम पेज पर आपका स्वागत है","description":"यह होम पेज है"},"AboutPage":{"title":"हमारे बारे में में आपका स्वागत है","description":"यह हमारे बारे में पेज है"},"ContactPage":{"title":"हमसे संपर्क करने के लिए आपका स्वागत है","description":"यह हमसे संपर्क करें पृष्ठ है"}}');
+
+/***/ })
+
+};
+;
